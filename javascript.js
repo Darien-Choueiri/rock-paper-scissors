73958cdc51da24efde94848a77e3b7e9f5ef22b3
Darien <darien.choueiri@gmail.com>
2022-06-18 16:04:41 -0400

    Add function to play a 5 round game
    Added function that keeps track of score and plays the game five times
    then declares a winner.

--- javascript.js
+++ javascript.js
@@ -16,37 +16,71 @@ function gameRound(computerSelection, playerSelection){
     let player = playerSelection.toLowerCase();
     let computer = computerSelection.toLowerCase();
     if (player === computer){
-        return "Tie!";
+        console.log("Tie!");
+        return 2;
     }
 
     if (player === "rock"){
         if (computer === "paper"){
-            return "You Lose! Paper beats Rock";
+            console.log("You Lose! Paper beats Rock");
+            return 0;
         }
         else if (computer === "scissors"){
-            return "You Win! Rock beats Scissors";
+            console.log("You Win! Rock beats Scissors");
+            return 1;
         }
     }
 
     if (player === "paper"){
         if (computer === "rock"){
-            return "You Win! Paper beats Rock";
+            console.log("You Win! Paper beats Rock");
+            return 1;
         }
         else if (computer === "scissors"){
-            return "You Lose! Scissors beats Paper";
+            console.log("You Lose! Scissors beats Paper");
+            return 0;
         }
     }
     
     if (player === "scissors"){
         if (computer === "rock"){
-            return "You Lose! Rock beats Scissors";
+            console.log("You Lose! Rock beats Scissors");
+            return 0;
         }
         else if (computer === "paper"){
-            return "You Win! Scissors beats Paper";
+            console.log("You Win! Scissors beats Paper");
+            return 1;
         }  
     }
 }
 
-let playerSelection = "ROCK";
-let computerSelection = computerPlay();
-console.log(gameRound(computerSelection, playerSelection));
+function game(){
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 0; i < 5; i++){
+
+        let computerSelection = computerPlay();
+        let playerSelection = prompt("Please enter Rock Paper or Scissors")
+        let x = gameRound(computerSelection, playerSelection);
+
+        if (x === 0){
+            computerScore++;
+        }
+        else if (x === 1){
+            playerScore++;
+        }
+    }
+    
+    if (playerScore > computerScore){
+        return "The Player Wins!"
+    }
+    else if (computerScore > playerScore){
+        return "The Computer Wins!"
+    }
+    else{
+        return "The game ends in a Tie!"
+    }
+}
+
+game()
