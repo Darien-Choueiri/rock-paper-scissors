4e4cbc290cb8908ff3d6721e93e733fec03d4ff8
Darien <darien.choueiri@gmail.com>
2022-06-18 15:45:12 -0400

    Add function to play a round of the game
    Created a function that is case insensitive to the string input that
    checks who wins the round of rock-paper-scissors

--- javascript.js
+++ javascript.js
@@ -12,3 +12,41 @@ function computerPlay(){
     }
 }
 
+function gameRound(computerSelection, playerSelection){
+    let player = playerSelection.toLowerCase();
+    let computer = computerSelection.toLowerCase();
+    if (player === computer){
+        return "Tie!";
+    }
+
+    if (player === "rock"){
+        if (computer === "paper"){
+            return "You Lose! Paper beats Rock";
+        }
+        else if (computer === "scissors"){
+            return "You Win! Rock beats Scissors";
+        }
+    }
+
+    if (player === "paper"){
+        if (computer === "rock"){
+            return "You Win! Paper beats Rock";
+        }
+        else if (computer === "scissors"){
+            return "You Lose! Scissors beats Paper";
+        }
+    }
+    
+    if (player === "scissors"){
+        if (computer === "rock"){
+            return "You Lose! Rock beats Scissors";
+        }
+        else if (computer === "paper"){
+            return "You Win! Scissors beats Paper";
+        }  
+    }
+}
+
+let playerSelection = "ROCK";
+let computerSelection = computerPlay();
+console.log(gameRound(computerSelection, playerSelection));
