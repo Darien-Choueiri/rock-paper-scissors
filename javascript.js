373bbac0d775f1b809c4de2993acecb936601098
Darien <darien.choueiri@gmail.com>
2022-06-18 15:24:35 -0400

    Add function to make the computer play
    The function will randomly return rock paper or scissors for a player to
    play against.

---
+++ javascript.js
@@ -0,0 +1,14 @@
+/*generates computer moves*/
+function computerPlay(){
+    let choice = Math.floor(Math.random() * 3);
+    if (choice === 0){
+        return "Rock";
+    }
+    else if (choice === 1){
+        return "Paper"
+    }
+    else if (choice === 2){
+        return "Scissors"
+    }
+}
+
